514e5174c3e443537646171135e8daa85cf0a648
gitaufar <zhafiraufar123@gmail.com>
2025-09-05 16:41:38 +0700

    feat: add cbi question

--- src/core/constants/const.ts
+++ src/core/constants/const.ts
@@ -1,1 +1,30 @@
 export const BASE_URL_MOOD = 'http://192.168.50.215:8000/predict_mood';
+
+export const CBI_QUESTION_PERSONAL = [
+  "Seberapa sering Anda merasa lelah?",
+  "Seberapa sering Anda merasa kelelahan secara fisik?",
+  "Seberapa sering Anda merasa kelelahan secara emosional?",
+  "Seberapa sering Anda berpikir: “Saya tidak sanggup lagi”?",
+  "Seberapa sering Anda merasa benar-benar terkuras?",
+  "Seberapa sering Anda merasa lemah dan rentan terhadap penyakit?"
+];
+
+export const CBI_QUESTION_WORK = [
+  "Apakah pekerjaan Anda terasa melelahkan secara emosional?",
+  "Apakah Anda merasa kelelahan karena pekerjaan Anda?",
+  "Apakah pekerjaan Anda membuat Anda frustrasi?",
+  "Apakah Anda merasa terkuras di akhir hari kerja?",
+  "Apakah Anda merasa lelah di pagi hari saat memikirkan hari kerja berikutnya?",
+  "Apakah setiap jam kerja terasa melelahkan bagi Anda?",
+  "Apakah Anda masih memiliki cukup energi untuk keluarga dan teman di waktu luang?"
+];
+
+export const CBI_QUESTION_CLIENT = [
+  "Apakah Anda merasa sulit untuk bekerja dengan klien?",
+  "Apakah bekerja dengan klien menguras energi Anda?",
+  "Apakah Anda merasa frustrasi saat bekerja dengan klien?",
+  "Apakah Anda merasa memberi lebih banyak daripada yang Anda terima saat bekerja dengan klien?",
+  "Apakah Anda merasa lelah bekerja dengan klien?",
+  "Apakah Anda kadang bertanya-tanya berapa lama lagi Anda sanggup terus bekerja dengan klien?"
+];
+
